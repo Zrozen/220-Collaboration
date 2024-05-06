ebca1017d19e30b3e73fa85970c23779c337b304
lifreese <lf200981@umconnect.umt.edu>
2024-05-06 14:47:02 -0600

    Update1-Idle+Attack(still broke)

--- Final Project/NonnaMafiaDraw.js
+++ Final Project/NonnaMafiaDraw.js
@@ -3,15 +3,18 @@ var background;
 //kitchen things
 var oven;
 var fridge;
+var sink;
+var door;
+
 var cabinets1;
 var cabinets2;
 var cabinets3;
-var door;
 
 
 var myAnimation;
 var walkPaths;
 var idlePaths;
+var attackPaths;
 
 
 function preload()
@@ -19,6 +22,19 @@ function preload()
     background = loadImage('Assets/Background/kitchenBackground.png')
     walkPaths = loadStrings('Assets/NonnaSprites/walking.txt');
     idlePaths = loadStrings('Assets/NonnaSprites/idle.txt');
+    attackPaths = loadStrings('Assets/NonnaSprites/attack.txt');
+
+    faucet = loadImage('Assets/Background/Faucet.jpg');
+    oven = loadImage('Assets/Background/oven.png');
+    door = loadImage('Assets/Background/Metal Door.jpg');
+    fridge = loadImage("Assets/Background/Fridge_icon_2.png");
+    sink = loadImage("Assets/Background/sink.jpg");
+    
+
+    cabinets1 = loadImage("Assets/Background/cabinet.png");
+    cabinets2 = loadImage("Assets/Background/cabinet.png");
+    cabinets3 = loadImage("Assets/Background/cabinet.png");
+    
     
 }
 
@@ -28,21 +44,11 @@ function setup()
     myAnimation = new animation(600,600,125,219);
     myAnimation.loadAnimation('idle', idlePaths);
     myAnimation.loadAnimation('walk', walkPaths);
-    //myAnimation.loadAnimation('attack', attackPaths);
+    myAnimation.loadAnimation('attack', attackPaths);
     myAnimation.rotation = 0;
 
-    oven = createSprite(600,200,'static');
-    oven.img = "Assets/Background/oven.png";
-    door = createSprite(100,100,'static');
-    door.img = "Assets/Background/Metal Door.jpg";
-    cabinets1 = createSprite(510,200,'static');
-    cabinets1.img = "Assets/Background/cabinet.png";
-    cabinets2 = createSprite(440,200,'static');
-    cabinets2.img = "Assets/Background/cabinet.png";
-    cabinets3 = createSprite(690,200,'static');
-    cabinets3.img = "Assets/Background/cabinet.png";
-    fridge = createSprite(790,175,'static');
-    fridge.img = "Assets/Background/Fridge_icon_2.png";
+   // sink = createSprite(275,165,'static');
+    //sink.img = "Assets/Background/sink.jpg";
     
 
 }
@@ -50,30 +56,48 @@ function setup()
 function draw()
 {
     image(background,0,0);
+    fill ('#874a3d');
+    rect(200,175,150,100);
+    image(faucet,225,60);
+    image(oven,542,138);
+    image(door,25,-10);
+    image(fridge,720,50);
+    image(sink,200,120);
+    image(cabinets1,480,138);
+    image(cabinets2,415,138);
+    image(cabinets3,645,138);
+
+
+    if(kb.pressing('d'))
+        {
+            myAnimation.updatePosition('forward');
+            myAnimation.drawAnimation('walk');  
+        }
+        else if(kb.pressing('a'))
+        {
+            myAnimation.updatePosition('reverse');
+            myAnimation.drawAnimation('walk');  
+               
+        }
+        else if(kb.pressing('w'))
+        {
+            myAnimation.updatePosition('up');
+            myAnimation.drawAnimation('walk');
+        }
+        else if(kb.pressing('s'))
+        {
+            myAnimation.updatePosition('down');
+            myAnimation.drawAnimation('walk');
+        }
+        else if (kb.pressing('x')) 
+        {
+        myAnimation.drawAnimation('attack');
+        }
+        else
+        {
+
+            myAnimation.drawAnimation('idle');
+        }
+        
 }
 
-if(kb.pressing('d'))
-{
-    myAnimation.updatePosition('forward');
-    myAnimation.drawAnimation('walk');  
-}
-else if(kb.pressing('a'))
-{
-    myAnimation.updatePosition('reverse');
-    myAnimation.drawAnimation('walk');  
-       
-}
-else if(kb.pressing('w'))
-{
-    myAnimation.updatePosition('up');
-    myAnimation.drawAnimation('walk');
-}
-else if(kb.pressing('s'))
-{
-    myAnimation.updatePosition('down');
-    myAnimation.drawAnimation('walk');
-}
-else
-{
-    myAnimation.drawAnimation('idle');
-}
