269036b9b3585119fd73b62c3df196256faf70e7
lifreese <lf200981@umconnect.umt.edu>
2024-05-07 14:51:49 -0600

    FixedAnimations/Particles/Text

--- Final Project/NonnaMafiaDraw.js
+++ Final Project/NonnaMafiaDraw.js
@@ -10,17 +10,31 @@ var cabinets1;
 var cabinets2;
 var cabinets3;
 
+//sign
+var tile;
 
+//NonnaAnimations
 var myAnimation;
 var walkPaths;
 var idlePaths;
 var attackPaths;
 
+//CorpseCrusher
+var corpse1;
+var corpse2;
+var corpse3;
+//particles
+const particles = [];
+var p;
+var m;
+var health = 100;
+
+
 
 function preload()
 {
     background = loadImage('Assets/Background/kitchenBackground.png')
-    walkPaths = loadStrings('Assets/NonnaSprites/walking.txt');
+    walkPaths = loadStrings('Assets/NonnaSprites/idle.txt')
     idlePaths = loadStrings('Assets/NonnaSprites/idle.txt');
     attackPaths = loadStrings('Assets/NonnaSprites/attack.txt');
 
@@ -29,6 +43,7 @@ function preload()
     door = loadImage('Assets/Background/Metal Door.jpg');
     fridge = loadImage("Assets/Background/Fridge_icon_2.png");
     sink = loadImage("Assets/Background/sink.jpg");
+    tile = loadImage("Assets/Background/redTile.jpg")
     
 
     cabinets1 = loadImage("Assets/Background/cabinet.png");
@@ -47,17 +62,24 @@ function setup()
     myAnimation.loadAnimation('attack', attackPaths);
     myAnimation.rotation = 0;
 
-   // sink = createSprite(275,165,'static');
-    //sink.img = "Assets/Background/sink.jpg";
+    //corpses
+    corpse1 = createSprite(200,600,'static');
+    corpse1.img = "Assets/Body.png";
+    corpse2 = createSprite(300,300,'static');
+    corpse2.img = "Assets/Body.png";
+    corpse3 = createSprite(800,300,'static');
+    corpse3.img = "Assets/Body.png";
     
 
 }
 
 function draw()
 {
+    //kitchen decor
     image(background,0,0);
     fill ('#874a3d');
     rect(200,175,150,100);
+    image(tile,415,10);
     image(faucet,225,60);
     image(oven,542,138);
     image(door,25,-10);
@@ -66,6 +88,38 @@ function draw()
     image(cabinets1,480,138);
     image(cabinets2,415,138);
     image(cabinets3,645,138);
+
+    //text
+    textSize(50);
+    textFont('blackletter');
+    fill('black');
+    text('Mafia Nona',450,50);
+    textSize(24);
+    text('Help Nona Clean the Kitchen!',420,85);
+    textSize(20);
+    text('WASD to move, X to attack!',450,115);
+
+    //particles
+    function createParticles(x,y)
+    {
+    for (let m = 0; m < 5; m++) 
+    {
+        let p = new particle(x,y);
+        particles.push(p);
+    }
+    for (let m = particles.length - 1; m >= 0; m--) 
+    {
+        particles[m].update();
+        particles[m].show();
+
+        if (particles[m].finished())
+        {
+          // remove this particle
+          particles.splice(m, 1);
+        }
+    }
+    }
+
 
 
     if(kb.pressing('d'))
@@ -92,6 +146,52 @@ function draw()
         else if (kb.pressing('x')) 
         {
         myAnimation.drawAnimation('attack');
+        if (corpse1 != null)
+            {
+                if (dist(myAnimation.getCurrentAnimation().position.x, myAnimation.getCurrentAnimation().position.y, corpse1.position.x, corpse1.position.y) < 200) 
+                {
+                createParticles(corpse1.position.x, corpse1.position.y);
+                //console.log("destroy");
+                health -= 1;
+                    
+                    if(health <= 0)
+                    {
+                    corpse1.remove();
+                    corpse1 = null;
+                    }
+                }
+            }
+        if (corpse2 != null)
+            {
+                if (dist(myAnimation.getCurrentAnimation().position.x, myAnimation.getCurrentAnimation().position.y, corpse2.position.x, corpse2.position.y) < 200) 
+                 {
+                    createParticles(corpse2.position.x, corpse2.position.y);
+                    //console.log("destroy");
+                    health -= 1;
+                        
+                    if(health <= 0)
+                    {
+                    corpse2.remove();
+                    corpse2 = null;
+                    }
+                }
+             }
+             if (corpse3 != null)
+                {
+                    if (dist(myAnimation.getCurrentAnimation().position.x, myAnimation.getCurrentAnimation().position.y, corpse3.position.x, corpse3.position.y) < 200) 
+                     {
+                        createParticles(corpse3.position.x, corpse3.position.y);
+                        //console.log("destroy");
+                        health -= 1;
+                            
+                        if(health <= 0)
+                        {
+                        corpse3.remove();
+                        corpse3 = null;
+                        }
+                    }
+                 }
+    
         }
         else
         {
---
+++ Final Project/constructors/corpse.js
@@ -0,0 +1,16 @@
+class corpse
+{
+    constructor(img,x,y,diameter)
+    {
+        this.myImage = img;
+        this.x = x;
+        this.y = y;
+        this.diameter = diameter
+    }
+
+    drawImage()
+    {
+        image(this.myImage,this.x,this.y,this.diameter)
+    }
+
+}
